ad223a77665e40c1e3901a526b1aa6caac409220
xurui <xurui1@modelbest.onaliyun.com>
2026-06-30 12:25:44 +0800

    modify test_forced_aligner_injection

--- tests/config/test_forced_aligner_injection.py
+++ tests/config/test_forced_aligner_injection.py
@@ -1,5 +1,4 @@
 import pytest
-
 import vllm_omni.config.pipeline_registry  # noqa: F401  (populate registry)
 from vllm_omni.config.pipeline_registry import OMNI_PIPELINES as _PIPELINE_REGISTRY
 from vllm_omni.config.stage_config import (
